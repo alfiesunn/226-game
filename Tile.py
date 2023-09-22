cbbf66b61ca63c562d511f9875023da0a8fd8268
alfiesunn <ICS21-061@camosun.bc.ca>
2023-09-21 23:11:30 -0700

    Added Tile class in the file

--- Tile.py
+++ Tile.py
@@ -1,7 +1,9 @@
+from Treasure import Treasure
+
 class Tile:
-    def __init__(self):
-        self.description = '.'
-        self.treasure = None
+    def __init__(self, description='.', treasure: Treasure = None):
+        self.description = description
+        self.treasure = treasure
 
     def __str__(self):
         return f'{self.description}{self.treasure}'
