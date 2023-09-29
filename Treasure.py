1320ea9c98216bec9614a6c81c53c6923f42677b
alfiesunn <ICS21-061@camosun.bc.ca>
2023-09-29 02:03:17 -0700

    Added the raise of ValueError

--- Treasure.py
+++ Treasure.py
@@ -1,7 +1,9 @@
 class Treasure:
-    def __init__(self, description='$', value=None):
+    def __init__(self, value=None, description='$'):
+        if not isinstance(value, int) or value is None:
+            raise ValueError("Value is required.")
         self.value = value
         self.description = description
 
     def __str__(self):
-        return f'{self.description}{self.value}'
+        return self.description
