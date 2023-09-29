396d8de5f1a785e7f998b19ace81c86500ed4898
alfiesunn <ICS21-061@camosun.bc.ca>
2023-09-29 02:02:35 -0700

    Updated add_player method

--- Tile.py
+++ Tile.py
@@ -1,10 +1,17 @@
 from Treasure import Treasure
 
 class Tile:
-    def __init__(self, description='.', treasure: Treasure = None):
+    def __init__(self, description: str = '.', treasure: Treasure = None):
         self.description = description
         self.treasure = treasure
+        self.player = None
 
     def __str__(self):
-        return f'{self.description}{self.treasure}'
+        if self.treasure:
+            return self.treasure.description
+        else:
+            return self.treasure    # return default value
+
+    def add_player(self, player):
+        self.player = player    # add a player to the tile
 
