007783316d267a7036f70122c69ba4130c2cb96c
alfiesunn <ICS21-061@camosun.bc.ca>
2023-09-30 01:45:52 -0700

    Edited the view logic

--- View.py
+++ View.py
@@ -2,9 +2,8 @@ from Board import Board
 
 def display(board: Board):
     for row in board.board:
-        for tile in row:
-            if tile.treasure:   # check if the tile contain a treasure, if not contains, return the tail
-                print(tile.treasure.description, end=" ")
-            else:
-                print(tile.description, end=" ")
+        for tile in row:   # check if the tile contain a treasure, if not contains, return the tail
+            print(tile, end=" ")
+            # else:
+            #     print(tile.description, end=" ")
         print()
