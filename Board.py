f32ca4cf7f00887e46891e5aba925a765d48746e
alfiesunn <ICS21-061@camosun.bc.ca>
2023-10-01 23:03:09 -0700

    Updated the comments

--- Board.py
+++ Board.py
@@ -5,9 +5,10 @@ import random
 
 class Board:
     def __init__(self, n, t, min_val, max_val, max_players):
-        self.players = {}
-        self.n = n
-        self.max_players = max_players
+        self.players = {}       # set a dictionaries for players
+        self.n = n              # set board size
+        self.max_players = max_players      # set maximum number of players
+
         # Handle Invalid Input
         # check if the value is match
         if not isinstance(n, int) or not isinstance(t, int) or not isinstance(min_val, int) or not isinstance(max_val, int):
@@ -20,8 +21,8 @@ class Board:
         # Place treasures randomly
         for _ in range(t):
             while True:
-                x = random.randint(0, n - 1)    # random coordinate
-                y = random.randint(0, n - 1)
+                x = random.randint(0, n - 1)    # random x-coordinate
+                y = random.randint(0, n - 1)    # random y-coordinate
                 # if there is no treasure, place the treasure in it
                 if not self.board[y][x].treasure:
                     self.board[y][x].treasure = Treasure(random.randint(min_val, max_val))
@@ -36,18 +37,22 @@ class Board:
             raise ValueError("Value is bigger than maximum.")
         if self.board[x][y].player:
             raise ValueError("Tile already there.")
-        player = Player(name)
-        self.players[name] = {}
-        self.players[name]['location'] = (x, y)
-        self.players[name]['score'] = 0
 
-        self.board[x][y].add_player(player)
+        player = Player(name)                       # Create a new player object
+        self.players[name] = {}                     # Initialize the player dictionary entry
+        self.players[name]['location'] = (x, y)     # return player location
+        self.players[name]['score'] = 0             # return player score
 
+        self.board[x][y].add_player(player)         # Add the player to the board
 
+    """ This is move a player method for a specified diection"""
     def move_player(self, name, direction):
         if name not in self.players:
             raise ValueError("Players not found")
-        x, y = self.players[name]['location']
+
+        x, y = self.players[name]['location']       # Get the player's current location
+
+        # Change the player's location
         match direction:
             case 'U':
                 x -= 1
@@ -67,7 +72,7 @@ class Board:
         else:
             # Update player's position
             self.board[self.players[name]['location'][0]][self.players[name]['location'][1]].player = None
-            self.players[name]['location'] = (x, y)
+            self.players[name]['location'] = (x, y)     # setup the new location in the dictionary
             self.board[x][y].add_player(Player(name))
 
 
@@ -75,13 +80,13 @@ class Board:
         if self.board[x][y].treasure is not None:
             score_increment = self.board[x][y].treasure.value
             print(self.players[name])
-            self.players[name]['score'] += score_increment
+            self.players[name]['score'] += score_increment      # Update the player's score
             self.board[x][y].treasure = None        # Remove the treasure
 
-    # check the treasure got by user or not
+    # Check if there are any treasures left on the board
     def count_treasures(self):
         for row in self.board:
             for tile in row:
                 if tile.treasure:
-                    return True
+                    return True     # Return True if a treasure is found
             return False
