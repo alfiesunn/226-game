9150e3a0fcce532abf7bac04698c0f2cfa9d7750
alfiesunn <ICS21-061@camosun.bc.ca>
2023-10-26 23:58:44 -0700

    Updated the Client side

--- client.py
+++ client.py
@@ -1,14 +1,68 @@
 #!/usr/bin/python3.11
 from socket import socket, AF_INET, SOCK_STREAM
 from sys import argv
+import struct
+
 
 BUF_SIZE = 1024
 HOST = '127.0.0.1'
-PORT = 65432
+PORT = 12345
+CMD_MASK = 0b11110000
+UP = 0b00100000
+LEFT = 0b01000000
+RIGHT = 0b01100000
+DOWN = 0b00110000
+QUIT = 0b10000000
+GET = 0b11110000
+
+PLAYER_MASK = 0b00001100
+PLAYER1 = 0b0100
+PLAYER2 = 0b1000
+PLAYER1_NAME = '1'
+PLAYER2_NAME = '2'
+
+ERROR = b'E'
+OK = b'O'
+
 
 with socket(AF_INET, SOCK_STREAM) as sock:  # TCP socket
     sock.connect((HOST, PORT))   # Initiates 3-way handshake
     print('Client:', sock.getsockname())  # Client IP and port
+# s = socket(AF_INET, SOCK_STREAM)
+# s.connect((HOST, PORT))
+# print('Client:', s.getsockname())  # Client IP and port
+
+def start_client():
+    sock.sendall('id'.encode('utf-8'))
+    data_size = sock.recv(BUF_SIZE)
+    player_id = data_size.decode('utf-8')
+    print(player_id)
+
+    if player_id == 'Error: 2 players are already exist.':
+        quit()
+
+
+def main():
+    start_client()
+    while True:
+        message = input('(U)p (L)eft (R)ight (D)own (Q)uit?\n').upper()
+        if message == 'Q':
+            sock.sendall(struct.pack('!H', QUIT))
+            quit()
+        elif message == 'U':
+            sock.sendall(struct.pack('!H', UP))
+        elif message == 'L':
+            sock.sendall(struct.pack('!H', LEFT))
+        elif message == 'R':
+            sock.sendall(struct.pack('!H', RIGHT))
+        elif message == 'D':
+            sock.sendall(struct.pack('!H', DOWN))
+        else:
+            print('Invalid Input, Enter again.')
+
+
+if __name__ == "__main__":
+    main()
 
 
 
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from View import display
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 
 # 1 byte segments
-BUF_SIZE = 1
+BUF_SIZE = 1024
 HOST = ''
 PORT = 12345
 
@@ -25,6 +25,8 @@ class Game:
         self.player2 = self.board.add_player("2", random.randint(0, self.board.n - 1), random.randint(0, self.board.n - 1))
 
     def start(self):
+        player_id = ['1','2']
+
         with socket(AF_INET, SOCK_STREAM) as sock:  # TCP socket
             sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  # Details later
             sock.bind((HOST, PORT))  # Claim messages sent to port "PORT"
@@ -36,89 +38,85 @@ class Game:
                 with sc:
                     # Receive the input from the client
                     data = sc.recv(BUF_SIZE)
+                    print(data.decode('utf-8'))
 
-                    # Convert the data to a bit string
-                    number = int.from_bytes(data, byteorder='big')
-                    bit_string = '{0:b}'.format(number)
-
-                    # print(bit_string)
-                    # print(number)
-
-                    # Figure out which bits are set
-                    # for p in range(8):
-                    #     print(f'Bit {p} is {1 if number & 2 ** p > 0 else 0}')
-
-                    # Figure out if all four top bits are set
-                    # if number & 0xf0 == 0xf0:
-                    #     print('All four top bits are set')
-
-                    player_input = None
-                    user_input = None
-
-                    sent_score = b''
-                    for name, obj in self.board.players.items():
-                        # print(f"Player {name} the score is {board.players[name]['score']}")
-                        player_score = struct.pack('!H', int(obj['score']))
-                        sent_score += player_score
-                        print(f"Player {name} the score is {obj['score']}")
-                    sc.sendall(sent_score)
-
-                    # f0 is for direction, 0f is for player
-
-                    if number & 0x0f == 0x04:
-                        player_input = '1'
-                        print('Select Player 1')
-
-                    if number & 0x0f == 0x08:
-                        player_input = '2'
-                        print('Select Player 2')
-
-                    if number & 0xf0 == 0x20:
-                        user_input = 'U'
-                        print(display(self.board))
-                        print('Player Move Up')
-
-                    elif number & 0xf0 == 0x40:
-                        user_input = 'L'
-                        print(display(self.board))
-                        print('Player Move left')
-
-                    elif number & 0xf0 == 0x60:
-                        user_input = 'R'
-                        print(display(self.board))
-                        print('Player Move Right')
-
-                    elif number & 0xf0 == 0x30:
-                        user_input = 'D'
-                        print(display(self.board))
-                        print('Player Move Down')
-
-                    elif number & 0xf0 == 0xf0:
-                        user_input = 'G'
-                        print(display(self.board))
-                        sc.sendall(str(display(self.board)).encode('utf-8'))
-                        print('Showing board')
-
-                    elif number & 0xf0 == 0x80:
-                        user_input = 'Q'
-                        if user_input != 'Q':
-                            pass
+                    if data.decode('utf-8') == 'id':
+                        if len(player_id) > 0:
+                            sc.send(player_id[0].encode('utf-8'))
+                            player_id.pop(0)    # delete the player
+                            print(player_id)
                         else:
-                            print("Good Game My Friend!")
-                            print('Player Quit')
-                            exit()
-
-                    if user_input is not None or player_input is not None or number & 0xf0 == 0xf0:
-                        try:
-                            self.board.move_player(player_input, user_input)
-                        except ValueError as details:
-                            print(details)
+                            sc.send('Error: 2 players are already exist.'.encode('utf-8'))
                     else:
-                        print('Invalid input, disconnect right now.')
-                        # sc.close()
-                        break
+                        # Convert the data to a bit string
+                        number = int.from_bytes(data, byteorder='big')
 
-                    # Send the result to the client
+                        player_input = None
+                        user_input = None
+
+                        sent_score = b''
+                        for name, obj in self.board.players.items():
+                            # print(f"Player {name} the score is {board.players[name]['score']}")
+                            player_score = struct.pack('!H', int(obj['score']))
+                            sent_score += player_score
+                            print(f"Player {name} the score is {obj['score']}")
+                        sc.sendall(sent_score)
+
+                        # f0 is for direction, 0f is for player
+
+                        if number & 0x0f == 0x04:
+                            player_input = '1'
+                            print('Select Player 1')
+
+                        if number & 0x0f == 0x08:
+                            player_input = '2'
+                            print('Select Player 2')
+
+                        if number & 0xf0 == 0x20:
+                            user_input = 'U'
+                            print(display(self.board))
+                            print('Player Move Up')
+
+                        elif number & 0xf0 == 0x40:
+                            user_input = 'L'
+                            print(display(self.board))
+                            print('Player Move left')
+
+                        elif number & 0xf0 == 0x60:
+                            user_input = 'R'
+                            print(display(self.board))
+                            print('Player Move Right')
+
+                        elif number & 0xf0 == 0x30:
+                            user_input = 'D'
+                            print(display(self.board))
+                            print('Player Move Down')
+
+                        elif number & 0xf0 == 0xf0:
+                            user_input = 'G'
+                            print(display(self.board))
+                            sc.sendall(str(display(self.board)).encode('utf-8'))
+                            print('Showing board')
+
+                        elif number & 0xf0 == 0x80:
+                            user_input = 'Q'
+                            if user_input != 'Q':
+                                pass
+                            else:
+                                print("Good Game My Friend!")
+                                print('Player Quit')
+
+                        if user_input is not None or player_input is not None or number & 0xf0 == 0xf0:
+                            try:
+                                self.board.move_player(player_input, user_input)
+                            except ValueError as details:
+                                print(details)
+                        else:
+                            print('Invalid input, disconnect right now.')
+                            # sc.close()
+                            break
+
+                        # Send the result to the client
 
 
 
